86138ca438f86f567c840fa376026a8f1c0f75b7
Ali Noel <noelams03@gmail.com>
2024-09-16 13:38:35 -0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [textInputValue, setTextInputValue] = useState('')
+  const[listOfGoals, setListOfGoals] = useState([])
+
+  function goalInputHandler (enteredText){
+    setTextInputValue(enteredText)
+  }
+
+  const addGoalHandler = ()=>{
+    setListOfGoals(currentCourseGoals  => [...currentCourseGoals, textInputValue])
+    setTextInputValue('')
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder='Your Goals' 
+          style={styles.textInput} 
+          onChangeText={goalInputHandler}
+          value={textInputValue}
+        />
+        <Button title='Add Goals' onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goalsContainer}>
+        {
+          listOfGoals.map((goal, key)=><Text style={styles.goals} key={key}>{goal}</Text>)
+        }
+      </View>
     </View>
   );
 }
@@ -13,8 +36,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  }, 
+  inputContainer:{
+    flex:1,
+    flexDirection:  'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingBottom: 24,
+    borderBottomWidth:1,
+    borderBottomColor: '#cccccc'
   },
+  textInput:{
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width:'70%',
+    marginRight: 8,
+    padding: 8
+  },
+  goalsContainer:{
+    flex: 7
+  },
+  goals:{
+    padding:10,
+    margin:8,
+    backgroundColor:'#5e0acc',
+    borderRadius:6,
+    color:'white'
+  }
 });
